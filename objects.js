eb09ed6ed2f245ced40cc64c22ead39dffb9dbe1
Brett Wilson <wilson.3458@osu.edu>
2020-05-07 07:43:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,6 @@
-var playlist = {artist:song};
+var playlist = {
+  artist: song
+};
 
 // updatePlaylist - add artist:song as a keypair to the playlist
 function updatePlaylist(playlist, artist, song){
