da94f27dd3636ff10b32a23ddc55a315092a3c2d
Brett Wilson <wilson.3458@osu.edu>
2020-05-07 07:41:06 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,13 @@
 var playlist = {artist:song};
+
+// updatePlaylist - add artist:song as a keypair to the playlist
+function updatePlaylist(playlist, artist, song){
+  playlist.assign({},playlist,{artist:song});
+  return playlist;
+}
+
+// removeFromPlaylist - remove artist:song as a keypair from the playlist
+function removeFromPlaylist(playlist, artist){
+  delete platlist.artist;
+  return playlist;
+}
